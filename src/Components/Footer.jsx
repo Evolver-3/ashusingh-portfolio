ac0c6ab376155ddbdc7bb7a51880af83d76be42b
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-21 21:10:42 +0530

    Footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-col text-foreground items-center justify-center gap-2'>
+      <h2 className='text-xl font-semibold'>Designed and Coded by <span className='text-2xl'>Ashu.</span></h2>
+
+      <h3>© {new Date().getFullYear()}  All rights reserved.</h3>
+
+      <h2>Open to internships and freelance opportunities.</h2>
+
+      <h2>Kanpur U.P India</h2>
+      
+
+    </div>
   )
 }
 
