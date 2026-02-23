9bae47fa6e9d48dc5d11d9da3b600ec0691f2268
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-23 18:34:31 +0530

    overflow issue fixed

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,7 +2,7 @@ import { motion } from "motion/react"
 
 const Container = ({ children, className = "" }) => {
   return (
-    <div className={`max-w-7xl mx-auto w-full bg-background shadow-acer ${className} transition-colors duration-500`}>
+    <div className={`max-w-7xl mx-auto w-full bg-background shadow-acer ${className} transition-colors duration-500 overflow-hidden`}>
         {children}
 
     </div>
