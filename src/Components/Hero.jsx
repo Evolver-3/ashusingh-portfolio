932ad8ef176fe643e1c2f466256f321197e690f5
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-23 20:19:07 +0530

    some minor

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -16,49 +16,48 @@ const Hero = () => {
  
   
   return (
-    <div className='w-full bg-background '>
+    <div className='w-full bg-background my-25 md:my-45 '>
+      <div className='flex flex-col md:flex-row items-center justify-between overflow-hidden sm:gap-5 '>
 
-      <div className='flex flex-col overflow-hidden '>
-
-        <motion.div className='pt-40 flex flex-col gap-4 text-foreground mx-10 pb-20'
+        <motion.div className='md:w-1/2 flex flex-col gap-5 sm:gap-8 text-foreground order-2 md:order-1 items-center sm:items-baseline sm:ml-10 justify-center'
         variants={containerVariant}
         initial="hidden"
         whileInView="show"
         viewport={{once:true}}>
 
           <HideOut>
-            <h2 className='relative text-7xl font-bold overflow-hidden'>
+            <h2 className=' text-5xl sm:text-7xl font-bold overflow-hidden'>
              Hi,I'm Ashu<span className='text-fullColor'>{"."}</span>
           </h2>
           </HideOut>
 
           <HideOut>
             <h2
-            className='text-3xl relative overflow-hidden'>I'm a <span className='text-fullColor text-4xl'>Frontend Developer</span>
+            className='text-xl sm:text-3xl  overflow-hidden'>I'm a <span className='text-fullColor  text-2xl sm:text-4xl'>Frontend Developer</span>
             </h2>
           </HideOut>
 
           <HideOut>
             <h2
-            className='text-2xl max-w-xl leading-tight relative'>A 25 year guy who like to build cool looking websites and deploy them.
+            className='text-xl sm:text-2xl max-w-xl leading-tight  '>A 25 year guy who like to build cool looking websites and deploy them.
             </h2>
           </HideOut>
 
           <HideOut>
             <button
-            className='relative mt-5 ml-2 overflow-hidden w-fit h-fit  rounded-md px-4 py-1.5 text-3xl text-white bg-fullColor border-2 border-dashed border-black font-semibold uppercase transition-all duration-300 hover:-translate-x-2 hover:-translate-y-2 hover:rounded-xl hover:shadow-[4px_4px_0px_var(--color-foreground)] active:translate-x-0 active:translate-y-0 active:rounded-md active:shadow-none'>
+            className='relative mt-5 ml-2 overflow-hidden w-fit h-fit  rounded-md px-4 py-1.5 text-3xl text-white bg-fullColor border-2 border-dashed border-black font-semibold uppercase transition-all duration-300 hover:-translate-x-2 hover:-translate-y-2 hover:rounded-xl hover:shadow-[4px_4px_0px_var(--color-foreground)] active:translate-x-0 active:translate-y-0 active:rounded-md active:shadow-none '>
             <span className=''>Contact me.</span>
           </button>
           </HideOut>
         </motion.div>
 
-        <div>
+        <div className='order-1 md:order-2 size-70 bg-red-500 rounded-full'>
+        
+        
 
         </div>
-
       
       </div>
-
     </div>
   )
 }
