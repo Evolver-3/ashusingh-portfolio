dd9b4942b291b8ca528d36a18626bf59436aa5e5
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-28 21:51:39 +0530

    feat: refactor Container and Education components, remove unused components

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,7 +2,11 @@ import { motion } from "motion/react"
 
 const Container = ({ children, className = "" }) => {
   return (
-    <div className={`max-w-7xl mx-auto w-full bg-background shadow-acer ${className} transition-colors duration-500 overflow-hidden`}>
+    <div className={`max-w-5xl mx-auto w-full bg-background  ${className} transition-colors duration-500 overflow-hidden flex flex-col gap-10`}>
+{/* 
+      <div className="absolute border-l border-r border-neutral-300 h-full w-px">
+        </div>
+      */}
         {children}
 
     </div>
--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PageReuse from './comp/PageReuse'
 
 const eduCards=[
   {
@@ -24,24 +25,44 @@ const eduCards=[
 
 const Education = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-15'>
-      <h2 className='relative text-5xl text-foreground font-semibold'>Education</h2>
-      <div className='w-full px-10 flex flex-col md:flex-row gap-20 sm:gap-10 my-15'>
+   <PageReuse text={"Education"}>
+    <div className='flex flex-col gap-5'>
+      {
+        eduCards.map((card)=>(
+          <div 
+          key={card.id}
+          className='w-full flex flex-col gap-2 md:gap-5 md:px-20'>
+             <div className='flex gap-4 items-center text-foreground '>
+              <Edulogo/>
+        
+              <h2 className='text-xl leading-snug font-bold '>
+                {card.college}
+              </h2>
+              </div>
 
-        {eduCards.map((card)=>(
-          <div key={card.id} className='w-full rounded-3xl bg-bdr p-5 flex items-center gap-5 px-10 sm:px-5'>
-            
-            <img src={'/aktu logo.png'} alt='aktu logo' className='size-20 rounded-full '/>
-
-            <div className='flex flex-col text-white'>
-              <h2 className='text-xl'>{card.text} <span>({card.year})</span></h2>
-              <p>{card.college}</p>
-            </div>
+              <div
+              className='flex gap-1 pl-11 flex-col text-lg leading-tight text-textxl'> 
+                <h2 className=''>{card.text}</h2>
+                
+                <h2 >{card.year}</h2>
+              </div>
+         
           </div>
-        ))}
-      </div>
+        ))
+      }
     </div>
+   </PageReuse>
   )
 }
 
 export default Education
+
+const Edulogo=()=>{
+  return(
+    <div className=' p-1 rounded-md bg-smallcard text-foreground border-borderlight ring-1 hover:bg-hovered transition-colors duration-200 '>
+      <svg
+      className='size-5'
+      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-graduation-cap size-4" aria-hidden="true"><path d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"></path><path d="M22 10v6"></path><path d="M6 12.5V16a6 3 0 0 0 12 0v-3.5"></path></svg>
+    </div>
+  )
+}
